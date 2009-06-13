c6c5781a590e3eb618e9239144c702c27a48590d
Sean Huber <shuber@huberry.com>
2009-06-13 16:59:36 -0700

    Add nestable tests

--- test/nestable_test.rb
+++ test/nestable_test.rb
@@ -1,1 +1,49 @@
 require 'test_helper'
+
+module Nestable
+  module TheoryWithValidation
+    def self.validate_nestable_options(options)
+      options
+    end
+  end
+  
+  module TheoryWithoutValidation
+  end
+end
+
+class NestableTest < Test::Unit::TestCase
+  
+  def test_should_add_a_nestable_method_to_active_record
+    assert ActiveRecord::Base.respond_to?(:nestable)
+  end
+  
+  def test_should_not_raise_a_name_error_if_theory_exists
+    ActiveRecord::Base.nestable
+  end
+  
+  def test_should_raise_a_name_error_if_theory_does_not_exist
+    assert_raises(NameError) { ActiveRecord::Base.nestable :theory => 'invalid' }
+  end
+  
+  def test_should_raise_a_no_method_error_if_theory_does_not_have_a_validate_nestable_options_method
+    assert_raises(NoMethodError) { ActiveRecord::Base.nestable :theory => 'theory_without_validation' }
+  end
+  
+  def test_should_add_a_cattr_accessor_for_nestable_options
+    assert !ActiveRecord::Base.respond_to?(:nestable_options)
+    ActiveRecord::Base.nestable
+    assert ActiveRecord::Base.respond_to?(:nestable_options)
+  end
+  
+  def test_should_include_theory
+    assert !ActiveRecord::Base.include?(Nestable::TheoryWithValidation)
+    ActiveRecord::Base.nestable :theory => 'theory_with_validation'
+    assert ActiveRecord::Base.include?(Nestable::TheoryWithValidation)
+  end
+  
+  def test_should_add_the_class_to_nestable_options
+    ActiveRecord::Base.nestable
+    assert_equal ActiveRecord::Base, ActiveRecord::Base.nestable_options[:class]
+  end
+  
+end
