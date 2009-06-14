18941b121441555ff789972ed37a37ac615c69a0
Sean Huber <shuber@huberry.com>
2009-06-14 15:08:04 -0700

    Add methods that need to be overwritten to set

--- lib/nestable/set.rb
+++ lib/nestable/set.rb
@@ -11,5 +11,20 @@ module Nestable
       { :left_column => :lft, :right_column => :rgt }.merge(Nestable::Tree.process_options!(options))
     end
     
+    def ancestors
+    end
+    
+    def descendants
+    end
+    
+    def leaves
+    end
+    
+    def root
+    end
+    
+    def siblings
+    end
+    
   end
 end
