546e3ba7d5c5d33e44b9801801ff0c93be8fb0d5
Sean Huber <shuber@huberry.com>
2009-06-15 23:49:19 -0700

    Always add :class to nestable_options

--- lib/nestable.rb
+++ lib/nestable.rb
@@ -13,11 +13,12 @@ module Nestable
   #
   # The default <tt>:theory</tt> is <tt>:tree</tt>
   def nestable(options = {})
+    options = { :class => self }.merge(options)
     theory = "Nestable::#{(options[:theory] ||= :tree).to_s.classify}".constantize
     missing_methods = Nestable::Interface.instance_methods.reject { |method| theory.method_defined?(method) }
     raise NotImplementedError.new("#{theory} must implement the following Nestable methods: #{missing_methods.sort.to_sentence}") unless missing_methods.empty?
     cattr_accessor :nestable_options
-    self.nestable_options = theory.respond_to?(:process_options!) ? theory.process_options!({ :class => self }.merge(options)) : options
+    self.nestable_options = theory.respond_to?(:process_options!) ? theory.process_options!(options) : options
     include theory
   end
   
